cc81be4c7a53281bbcd0ff027a9cdfe0dc1ea4f7
Kanda Yuki <kand4y@gmail.com>
2023-08-15 14:36:40 +0900

    remove: ビルド設定コメントアウト

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "./",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        test: resolve(__dirname, "pages/test.html"),
-        about: resolve(__dirname, "pages/about.html"),
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, "index.html"),
+  //       test: resolve(__dirname, "pages/test.html"),
+  //       about: resolve(__dirname, "pages/about.html"),
+  //     },
+  //   },
+  // },
 });
